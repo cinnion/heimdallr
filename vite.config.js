03c681a7b7cd152b542f9a92f84610531e9d1349
Douglas Needham <cinnion@gmail.com>
2025-02-01 21:46:26 -0500

    Adjust vite.config.js to get generated urls to not use ipv6.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: 'wing-1.home.ka8zrt.com',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
